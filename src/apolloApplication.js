7b7675e18b9981b8d1a5da3d214b55de6486ae3b
sheshenk <23496040+sheshenk@users.noreply.github.com>
2022-05-23 15:13:05 +0800

    Update apolloApplication.js

--- src/apolloApplication.js
+++ src/apolloApplication.js
@@ -4,8 +4,6 @@ import { FolderModule } from "../types/Folder.js";
 import { LessonModule } from "../types/Lesson.js";
 import { ModuleModule } from "../types/Module.js";
 import { ModuleTakenModule } from "../types/ModuleTaken.js";
-// import { MutationModule } from "../modules/Mutation.js";
-// import { QueryModule } from "../modules/Query.js";
 import { RoleModule } from "../types/Role.js";
 import { SignInResponseModule } from "../types/SignInResponse.js";
 import { StudentModule } from "../types/Student.js";
@@ -18,8 +16,6 @@ export const apolloApplication = createApplication({
     LessonModule, 
     ModuleModule, 
     ModuleTakenModule, 
-    // MutationModule, 
-    // QueryModule, 
     RoleModule, 
     SignInResponseModule, 
     StudentModule, 
