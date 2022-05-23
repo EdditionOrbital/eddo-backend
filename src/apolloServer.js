b6046027439ce359f957529100e9a91221996753
sheshenk <23496040+sheshenk@users.noreply.github.com>
2022-05-23 19:08:55 +0800

    removed redundant code

--- src/apolloServer.js
+++ src/apolloServer.js
@@ -22,7 +22,6 @@ const apolloServer = new ApolloServer({
         console.log(req.body.operationName)
         if (whitelisted.includes(req.body.operationName)) return {}
         const token = req.headers.authorization || ''
-        // const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkEwMjMzMjEwWCIsImlhdCI6MTY1Mjg3MDg3Nn0.-eezMHGOCtt4F3yvmL5R72nnEBqN3ttESwPGZV0LLwE'
         if (!token.includes('Bearer ')) throw new AuthenticationError("Token must use Bearer format.")
         const user = getUser(token.split(' ')[1])
         if (!user) throw new AuthenticationError("You must be logged in!")
