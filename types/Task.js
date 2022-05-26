14e681cd2dfd60b261914d06e6592c6a87d348e6
sheshenk <23496040+sheshenk@users.noreply.github.com>
2022-05-26 22:05:16 +0800

    added create, update, delete resolvers for task

--- types/Task.js
+++ types/Task.js
@@ -12,30 +12,67 @@ export const TaskSchema = mongoose.Schema({
     date: { type: schemaTypes.String, required: false },
 })
 
-export const Task = mongoose.model('Task', StudentSchema)
+export const Task = mongoose.model('Task', TaskSchema)
 
 export const getAllTasks = () => Task.find({}).then(unpackMultipleDocuments).catch(err => console.log('Error while getting all tasks'))
-export const getUserTasks = (id) => Task.find({id:id}).then(unpackMultipleDocuments).catch(err => console.log('Error while getting student tasks'))
+export const getUserTasks = (id) => Task.find({studentId: id}).then(unpackMultipleDocuments).catch(err => console.log('Error while getting student tasks' + err))
 
 export const TaskModule = createModule({
+
   id: "task",
+
   typeDefs: gql`
     type Task {
+        _id: ID!
         studentId: String!
         title: String!
         status: String!
         date: String
     }
 
-
     type Query {
         currentUserTasks: [Task!]!
     }
+
+    type Mutation {
+        newTask(title: String, status: String): MutationResponse
+        updateTask(_id: ID!, title: String, status: String): MutationResponse
+        deleteTask(_id: ID!): MutationResponse
+    }
+
   `,
+  
   resolvers: {
       Query: {
-          currentUserTasks: (parent, args, context) => {
-            return getUserTasks(context.id)
+          currentUserTasks: async (parent, args, context) => {
+            const tasks = await getUserTasks(context.id)
+            if (tasks === undefined || tasks === null) return []
+            return tasks
+          }
+      },
+      Mutation: {
+          newTask: (parent, args, context) => {
+            const studentId = context.id
+            const { title, status } = args
+            const date = null
+            const resp = new Task({
+              studentId,
+              title,
+              status,
+              date
+            }).save().then(result => ({ completed: result._id}))
+            .catch(err => ({ error: err}))
+            return resp
+          },
+          updateTask: async (parent, args, context) => {
+            const { _id, title, status } = args
+            const doc = await Task.findOne({_id: _id })
+            doc.title = title
+            doc.status = status
+            return doc.save().then(result => ({ completed: result._id })).catch(err => ({ error: err })) 
+          },
+          deleteTask: (parent, args, context) => {
+            return Task.deleteOne({_id: args._id}).then(() => ({completed: 'Deleted'})).catch(err => ({error:err}))
           }
       }
   }
