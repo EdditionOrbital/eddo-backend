f3f4aed9cd0ec01ad77974aeb1e0daa1eb6b23d7
sheshenk <23496040+sheshenk@users.noreply.github.com>
2022-05-26 22:04:17 +0800

    added task and announcement to application

--- src/apolloApplication.js
+++ src/apolloApplication.js
@@ -4,10 +4,13 @@ import { FolderModule } from "../types/Folder.js";
 import { LessonModule } from "../types/Lesson.js";
 import { ModuleModule } from "../types/Module.js";
 import { ModuleTakenModule } from "../types/ModuleTaken.js";
+import { MutationResponse } from "../types/MutationResponse.js";
 import { RoleModule } from "../types/Role.js";
 import { SignInResponseModule } from "../types/SignInResponse.js";
 import { StudentModule } from "../types/Student.js";
+import { TaskModule } from "../types/Task.js";
 import { UserModule } from "../types/User.js";
+import { AnnouncementModule } from "../types/Announcement.js";
 
 export const apolloApplication = createApplication({
   modules: [
@@ -15,10 +18,13 @@ export const apolloApplication = createApplication({
     FolderModule, 
     LessonModule, 
     ModuleModule, 
+    MutationResponse,
     ModuleTakenModule, 
     RoleModule, 
     SignInResponseModule, 
     StudentModule, 
-    UserModule
+    UserModule,
+    TaskModule,
+    AnnouncementModule
   ],
 });
