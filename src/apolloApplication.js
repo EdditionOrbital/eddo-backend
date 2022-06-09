bac8ac946948587d5e97fe0318477759ac32e56b
Shashank Acharya <acharya.s@icloud.com>
2022-06-09 21:43:54 +0800

    Update apolloApplication.js

--- src/apolloApplication.js
+++ src/apolloApplication.js
@@ -4,14 +4,14 @@ import { FolderModule } from "../types/Folder.js";
 import { LessonModule } from "../types/Lesson.js";
 import { ModuleModule } from "../types/Module.js";
 import { ModuleTakenModule } from "../types/ModuleTaken.js";
-import { MutationResponse } from "../types/MutationResponse.js";
 import { RoleModule } from "../types/Role.js";
-import { SignInResponseModule } from "../types/SignInResponse.js";
 import { StudentModule } from "../types/Student.js";
 import { TaskModule } from "../types/Task.js";
 import { UserModule } from "../types/User.js";
 import { AnnouncementModule } from "../types/Announcement.js";
 import { StaffModule } from "../types/Staff.js";
+import { AdminModule } from "../types/Admin.js";
+import { HTTPResponseModule } from "../types/HTTPResponse.js";
 
 export const apolloApplication = createApplication({
   modules: [
@@ -19,14 +19,14 @@ export const apolloApplication = createApplication({
     FolderModule, 
     LessonModule, 
     ModuleModule, 
-    MutationResponse,
     ModuleTakenModule, 
     RoleModule, 
-    SignInResponseModule, 
     StudentModule, 
     UserModule,
     TaskModule,
     AnnouncementModule,
-    StaffModule
-  ],
+    StaffModule,
+    AdminModule,
+    HTTPResponseModule
+  ]
 });
