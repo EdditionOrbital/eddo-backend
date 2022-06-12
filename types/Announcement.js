26e5afbf2c49fd7f4fe4e83b67128c010036ca52
Shashank Acharya <acharya.s@icloud.com>
2022-06-12 10:50:40 +0800

    added author resolver to announcement

--- types/Announcement.js
+++ types/Announcement.js
@@ -1,6 +1,7 @@
 import { createModule, gql } from "graphql-modules";
 import { readAnnouncements } from "../db_functions/Announcement.js";
 import { readStudent } from "../db_functions/Student.js";
+import { readStaff } from "../db_functions/Staff.js";
 
 export const AnnouncementModule = createModule({
   id: "announcement",
@@ -12,6 +13,7 @@ export const AnnouncementModule = createModule({
 		content: String
 		date: String!
 		readBy: [String!]!
+		author: String
 	}
 
 	type Query {
@@ -19,14 +21,17 @@ export const AnnouncementModule = createModule({
 	}
   `,
   resolvers: {
-	  Query: {
-		  contextAnnouncements: async (_, __, context) => {
-			const student = await readStudent({id:context.id})
-			const moduleIds = student.modules.map(x => x.moduleId)
-			const announcements = await readAnnouncements()
-			if (!moduleIds.length) return []
-			return announcements.filter(a => (moduleIds.includes(a.moduleId) && a.readBy.includes(student.firstName)))
-		  }
-	  }
+	Announcement: {
+		author: (p) => readStaff({id: p.authorId})
+	},
+	Query: {
+		contextAnnouncements: async (_, __, context) => {
+		const student = await readStudent({id:context.id})
+		const moduleIds = student.modules.map(x => x.moduleId)
+		const announcements = await readAnnouncements()
+		if (!moduleIds.length) return []
+		return announcements.filter(a => (moduleIds.includes(a.moduleId) && a.readBy.includes(student.firstName)))
+		}
+	}
   }
 })
